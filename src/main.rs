71f9ee62e1ff9e84264491ec041421aec482aa2b
Alexis Flodev <floresfernandezalexishr@gmail.com>
2025-04-19 17:09:16 -0600

    Logging in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use log::{info, trace, warn, error, debug};
+use env_logger::Builder;
+
 fn main() {
-    println!("Hello, world!");
+    Builder::from_env(env_logger::Env::default().default_filter_or("info,rust_dns_proto=error")).init();
+
+    info!("Message with info level");
+    error!("Message with error level");
+    debug!("Message with debug level");
+    trace!("Message with trace level");
+    warn!("Message with warn level");
 }
